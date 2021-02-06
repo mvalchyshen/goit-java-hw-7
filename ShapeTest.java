43ec00fa5f6829c3f45dc0380bd29b03690888fd
UAValchyMa <mvalchyshen@gmail.com>
2021-02-06 11:34:51 +0200

    HW-7 newShapePrintName

---
+++ ShapeTest.java
@@ -0,0 +1,63 @@
+package main.java.ModuleSeven.HomeWork;
+
+public class ShapeTest {
+
+    public static void main(String[] args) {
+        Main shape = new Main();
+
+        shape.print(new Circle());
+        shape.print(new Quad());
+        shape.print(new Qube());
+        shape.print(new Triangle());
+    }
+}
+
+class Main {
+    public void print(Shape shape) {
+        System.out.println(shape.getName());
+    }
+}
+
+
+    abstract class Shape {
+        abstract String getName();
+
+
+
+    }
+
+      class Circle extends Shape {
+        @Override
+        String getName() {
+            return "Circle";
+        }
+    }
+
+    class Quad extends Shape {
+        @Override
+        String getName() {
+            return "Quad";
+        }
+    }
+
+    class Qube extends Shape {
+        @Override
+        String getName() {
+            return "Qube";
+        }
+    }
+
+    class Triangle extends Shape {
+        @Override
+        String getName() {
+            return "Triangle";
+        }
+    }
+    class Rhombus extends Shape {
+        @Override
+        String getName() {
+            return "Rhombus";
+        }
+    }
+
+
